89a9a517d5127f488796e39edebbe03830af7358
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-10-14 14:04:31 +0800

    Code update

--- server.R
+++ server.R
@@ -1,43 +1,29 @@
 
 function(input, output, session) {
 
-
   # pkgStream is a reactive expression that represents a stream of
   # new package download data; up to once a second it may return a
   # data frame of new downloads since the last update.
   pkgStream <- packageStream(session)
   
-  # Max age of data (5 minutes)
-  maxAgeSecs <- 60 * 5
-
   # pkgData is a reactive expression that accumulates previous
   # values of pkgStream, discarding any that are older than
   # maxAgeSecs.
-  pkgData <- packageData(pkgStream, maxAgeSecs)
+  pkgData <- packageData(pkgStream, 100)
   
-  # Record the time that the session started.
-  startTime <- as.numeric(Sys.time())
- 
   output$rate <- renderValueBox({
     # The downloadRate is the number of rows in pkgData since
     # either startTime or maxAgeSecs ago, whichever is later.
-    #elapsed <- as.numeric(Sys.time()) - startTime
-    #downloadRate <- nrow(pkgData()) / min(maxAgeSecs, elapsed)
-    
     valueBox(
       value = formatC(nrow(pkgData()), digits = 1, format = "f"),
-      subtitle = "Percent completed",
-      icon = icon("percent"),
-      color = "yellow" 
+      subtitle = "Percent completed",icon = icon("percent")
     )
   })
   
   output$count <- renderValueBox({
-    currentServerCnt   <- max(nrow(pkgData()), input$rateThreshold*1.50)
     valueBox(
-      value = currentServerCnt,
-      subtitle = "Time to complete",
-      icon = icon("clock-o")
+      value = max(nrow(pkgData()), input$rateThreshold*100),
+      subtitle = "Time to complete",icon = icon("clock-o")
     )
   })
   
