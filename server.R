cd4a372f375bf0d55a40ab29b1d8809053daa9d5
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-10-14 16:13:22 +0800

    Code update

--- server.R
+++ server.R
@@ -11,34 +11,6 @@ function(input, output, session) {
 # An empty prototype of the data frame we want to create
 prototype <- data.frame(date = character(), time = character(),size = numeric(), r_version = character(), r_arch = character(),r_os = character(), package = character(), version = character(),country = character(), ip_id = character(), received = numeric())
 
-# Connects to streaming log data for cran.rstudio.com and
-# returns a reactive expression that serves up the cumulative
-# results as a data frame
-packageStream <- function(session) {
-  # Connect to data source
-  sock <- socketConnection("cransim.rstudio.com", 6789, blocking = FALSE, open = "r")
-  # Clean up when session is over
-  session$onSessionEnded(function() {
-    close(sock)
-  })
-
-  # Returns new lines
-  newLines <- reactive({
-    invalidateLater(1000, session)
-    readLines(sock)
-  })
-
-  # Parses newLines() into data frame
-  reactive({
-    if (length(newLines()) == 0)
-      return()
-    read.csv(textConnection(newLines()), header=FALSE, stringsAsFactors=FALSE,
-      col.names = names(prototype)
-    ) %>% mutate(received = as.numeric(Sys.time()))
-  })
-}
-
-
   # pkgStream is a reactive expression that represents a stream of
   # new package download data; up to once a second it may return a
   # data frame of new downloads since the last update.
