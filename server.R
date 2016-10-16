68fe28c1c7c9e1ccee630160877718dec4db2786
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-10-16 15:24:19 +0800

    Code update

--- server.R
+++ server.R
@@ -123,7 +123,7 @@ function(input, output, session) {
     # We'll use these multiple times, so use short var names for
     # convenience.
     p_servercnt <- input$servercnt
-    #p_marketInterest <- input$marketInterest
+    p_marketInterest <- input$marketInterest
     #p_perceivedValue <- iput$perceivedValue
     #p_costtoDeliver <- input$costtoDeliver
     system('/home/cem/RabbitMQ/send.py ccc')
