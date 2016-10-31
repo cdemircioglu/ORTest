7233a7fd77718e4bc48721a9f72a9086a6cfdb5d
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-11-01 01:27:55 +0800

    Code update

--- server.R
+++ server.R
@@ -150,8 +150,8 @@ function(input, output, session) {
       lastmarketInterest <<- input$marketInterest
       lastperceivedValue <<- input$perceivedValue
       lastcosttoDeliver <<- input$costtoDeliver
-      #timeRequired <<- df_duration[which(df_duration$X2 == "ACCOUNTING"),1]
-      timeRequired <<- 7500
+      timeRequired <<- df_duration[which(df_duration$X2 == "ACCOUNTING"),1]
+      #timeRequired <<- 7500
       initialtimeRequired <<- timeRequired
     }
 
