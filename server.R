93467e9c6eb53b369d5468da658e963e5ea4f787
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-11-01 10:48:10 +0800

    Code update

--- server.R
+++ server.R
@@ -153,7 +153,7 @@ function(input, output, session) {
       lastperceivedValue <<- input$perceivedValue
       lastcosttoDeliver <<- input$costtoDeliver
       currentMarketInterest <- df_duration[which(df_duration$X2 == input$marketInterest),]
-      timeRequired <<- as.numeric(as.character(currentMarketInterest[1]))*1000
+      timeRequired <<- as.numeric(as.character(currentMarketInterest[1]))*250
       initialtimeRequired <<- timeRequired
     }
 
