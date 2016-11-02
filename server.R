79b195663c75642a6f1227e87143f5d6ff64176c
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-11-02 11:43:46 +0800

    Code update

--- server.R
+++ server.R
@@ -234,20 +234,20 @@ function(input, output, session) {
   }
   
   
-  cp <- function (n, h = 155, c. = c(67, 0), l = c(40, 95), power = 0.533333333333333, 
-                  fixup = TRUE, gamma = NULL, alpha = 1, ...) 
+  cp <- function (n, h = c(130, 30), c. = c(65, 0), l = c(45, 90), power = c(0.5, 1.5), fixup = TRUE, gamma = NULL, alpha = 1, ...) 
   {
     if (!is.null(gamma)) 
       warning("'gamma' is deprecated and has no effect")
     if (n < 1L) 
       return(character(0L))
+    h <- rep(h, length.out = 2L)
     c <- rep(c., length.out = 2L)
     l <- rep(l, length.out = 2L)
     power <- rep(power, length.out = 2L)
     rval <- seq(1, 0, length = n)
     rval <- hex(polarLUV(L = l[2L] - diff(l) * rval^power[2L], 
-                         C = c[2L] - diff(c) * rval^power[1L], H = h[1L]), fixup = fixup, 
-                ...)
+                         C = c[2L] - diff(c) * rval^power[1L], H = h[2L] - diff(h) * 
+                           rval), fixup = fixup, ...)
     if (!missing(alpha)) {
       alpha <- pmax(pmin(alpha, 1), 0)
       alpha <- format(as.hexmode(round(alpha * 255 + 1e-04)), 
