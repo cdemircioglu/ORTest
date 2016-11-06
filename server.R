8ae26b554409e5355e5036631563070791947416
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-11-06 13:54:06 +0800

    Code update

--- server.R
+++ server.R
@@ -402,7 +402,7 @@ function(input, output, session) {
          #breaks=21,
          main="",
          col=tcol,
-         ylim=c(0,10000),
+         ylim=c(0,5000),
          border=tcol,
          xlab="",
          cex.lab=tscale,
