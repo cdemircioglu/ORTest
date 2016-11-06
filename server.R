30ca64517c550fb1c253b28a7e2889656422a1fb
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-11-06 16:14:32 +0800

    Code update

--- server.R
+++ server.R
@@ -370,7 +370,7 @@ function(input, output, session) {
     
     valueBox(
       #value = customerCount(),
-      value = prettyNum(percent*100000, scientific=FALSE, big.mark=','),
+      value = prettyNum(floor(percent*100000), scientific=FALSE, big.mark=','),
       subtitle = "Customers scanned",
       icon = icon("users")
     )
