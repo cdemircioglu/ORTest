392f20dc8274eb78e70b948f5cda69bb80f950b0
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-11-09 18:50:28 +0800

    Code update

--- server.R
+++ server.R
@@ -387,7 +387,7 @@ function(input, output, session) {
     {
       valueBox(
         #value = customerCount(),
-        value = paste(prettyNum(floor(df$cmsisdn/1000), scientific=FALSE, big.mark=','),"K",sep=""),
+        value = paste(prettyNum(floor(df$cmsisdn/500), scientific=FALSE, big.mark=','),"K",sep=""),
         #value = df$cmsisdn,
         subtitle = "Customers within the market",
         icon = icon("users")
