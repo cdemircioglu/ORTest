b9ead93af3e26e1179e556858a65cfee0f50d0cf
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-11-22 01:05:03 +0800

    Code update

--- server.R
+++ server.R
@@ -238,81 +238,81 @@ function(input, output, session) {
     cmdString <- paste(cmdString,'</ShinnyParameters>"',sep="")
     
     # Send the message
-    system(cmdString)
+    #system(cmdString)
     
   })
   
-  # Color function
-  cx <- function (n, h = c(-243, 360), c = 91, l = c(61, 77), power = 0.833333333333333, 
-                  fixup = TRUE, gamma = NULL, alpha = 1, ...) 
-  {
-    if (!is.null(gamma)) 
-      warning("'gamma' is deprecated and has no effect")
-    if (n < 1L) 
-      return(character(0L))
-    h <- rep(h, length.out = 2L)
-    c <- c[1L]
-    l <- rep(l, length.out = 2L)
-    power <- rep(power, length.out = 2L)
-    rval <- seq(1, -1, length = n)
-    rval <- hex(polarLUV(L = l[2L] - diff(l) * abs(rval)^power[2L], 
-                         C = c * abs(rval)^power[1L], H = ifelse(rval > 0, h[1L], 
-                                                                 h[2L])), fixup = fixup, ...)
-    if (!missing(alpha)) {
-      alpha <- pmax(pmin(alpha, 1), 0)
-      alpha <- format(as.hexmode(round(alpha * 255 + 1e-04)), 
-                      width = 2L, upper.case = TRUE)
-      rval <- paste(rval, alpha, sep = "")
-    }
-    return(rval)
-  }
-
-  cn <- function (n, h = c(360, 293), c. = c(80, 26), l = c(34, 95), power = c(0.7, 1.3), fixup = TRUE, gamma = NULL, alpha = 1,...) 
-  {
-    if (!is.null(gamma)) 
-      warning("'gamma' is deprecated and has no effect")
-    if (n < 1L) 
-      return(character(0L))
-    h <- rep(h, length.out = 2L)
-    c <- rep(c., length.out = 2L)
-    l <- rep(l, length.out = 2L)
-    power <- rep(power, length.out = 2L)
-    rval <- seq(1, 0, length = n)
-    rval <- hex(polarLUV(L = l[2L] - diff(l) * rval^power[2L], 
-                         C = c[2L] - diff(c) * rval^power[1L], H = h[2L] - diff(h) * 
-                           rval), fixup = fixup, ...)
-    if (!missing(alpha)) {
-      alpha <- pmax(pmin(alpha, 1), 0)
-      alpha <- format(as.hexmode(round(alpha * 255 + 1e-04)), 
-                      width = 2L, upper.case = TRUE)
-      rval <- paste(rval, alpha, sep = "")
-    }
-    return(rval)
-  }
-
+#  # Color function
+#  cx <- function (n, h = c(-243, 360), c = 91, l = c(61, 77), power = 0.833333333333333, 
+#                  fixup = TRUE, gamma = NULL, alpha = 1, ...) 
+#  {
+#    if (!is.null(gamma)) 
+#      warning("'gamma' is deprecated and has no effect")
+#    if (n < 1L) 
+#      return(character(0L))
+#    h <- rep(h, length.out = 2L)
+#    c <- c[1L]
+#    l <- rep(l, length.out = 2L)
+#    power <- rep(power, length.out = 2L)
+#    rval <- seq(1, -1, length = n)
+#    rval <- hex(polarLUV(L = l[2L] - diff(l) * abs(rval)^power[2L], 
+#                         C = c * abs(rval)^power[1L], H = ifelse(rval > 0, h[1L], 
+#                                                                 h[2L])), fixup = fixup, ...)
+#    if (!missing(alpha)) {
+#      alpha <- pmax(pmin(alpha, 1), 0)
+#      alpha <- format(as.hexmode(round(alpha * 255 + 1e-04)), 
+#                      width = 2L, upper.case = TRUE)
+#      rval <- paste(rval, alpha, sep = "")
+#    }
+#    return(rval)
+#  }
   
-  cp <- function (n, h = c(130, 30), c. = c(65, 0), l = c(45, 90), power = c(0.5, 1.5), fixup = TRUE, gamma = NULL, alpha = 1, ...) 
-  {
-    if (!is.null(gamma)) 
-      warning("'gamma' is deprecated and has no effect")
-    if (n < 1L) 
-      return(character(0L))
-    h <- rep(h, length.out = 2L)
-    c <- rep(c., length.out = 2L)
-    l <- rep(l, length.out = 2L)
-    power <- rep(power, length.out = 2L)
-    rval <- seq(1, 0, length = n)
-    rval <- hex(polarLUV(L = l[2L] - diff(l) * rval^power[2L], 
-                         C = c[2L] - diff(c) * rval^power[1L], H = h[2L] - diff(h) * 
-                           rval), fixup = fixup, ...)
-    if (!missing(alpha)) {
-      alpha <- pmax(pmin(alpha, 1), 0)
-      alpha <- format(as.hexmode(round(alpha * 255 + 1e-04)), 
-                      width = 2L, upper.case = TRUE)
-      rval <- paste(rval, alpha, sep = "")
-    }
-    return(rval)
-  }
+#  cn <- function (n, h = c(360, 293), c. = c(80, 26), l = c(34, 95), power = c(0.7, 1.3), fixup = TRUE, gamma = NULL, alpha = 1,...) 
+#  {
+#    if (!is.null(gamma)) 
+#      warning("'gamma' is deprecated and has no effect")
+#    if (n < 1L) 
+#      return(character(0L))
+#    h <- rep(h, length.out = 2L)
+#    c <- rep(c., length.out = 2L)
+#    l <- rep(l, length.out = 2L)
+#    power <- rep(power, length.out = 2L)
+#    rval <- seq(1, 0, length = n)
+#    rval <- hex(polarLUV(L = l[2L] - diff(l) * rval^power[2L], 
+#                         C = c[2L] - diff(c) * rval^power[1L], H = h[2L] - diff(h) * 
+#                           rval), fixup = fixup, ...)
+#    if (!missing(alpha)) {
+#      alpha <- pmax(pmin(alpha, 1), 0)
+#      alpha <- format(as.hexmode(round(alpha * 255 + 1e-04)), 
+#                      width = 2L, upper.case = TRUE)
+#      rval <- paste(rval, alpha, sep = "")
+#    }
+#    return(rval)
+#  }
+  
+  
+#  cp <- function (n, h = c(130, 30), c. = c(65, 0), l = c(45, 90), power = c(0.5, 1.5), fixup = TRUE, gamma = NULL, alpha = 1, ...) 
+#  {
+#    if (!is.null(gamma)) 
+#      warning("'gamma' is deprecated and has no effect")
+#    if (n < 1L) 
+#      return(character(0L))
+#    h <- rep(h, length.out = 2L)
+#    c <- rep(c., length.out = 2L)
+#    l <- rep(l, length.out = 2L)
+#    power <- rep(power, length.out = 2L)
+#    rval <- seq(1, 0, length = n)
+#    rval <- hex(polarLUV(L = l[2L] - diff(l) * rval^power[2L], 
+#                         C = c[2L] - diff(c) * rval^power[1L], H = h[2L] - diff(h) * 
+#                           rval), fixup = fixup, ...)
+#    if (!missing(alpha)) {
+#      alpha <- pmax(pmin(alpha, 1), 0)
+#      alpha <- format(as.hexmode(round(alpha * 255 + 1e-04)), 
+#                      width = 2L, upper.case = TRUE)
+#      rval <- paste(rval, alpha, sep = "")
+#    }
+#    return(rval)
+#  }
   
     
   #######OUTPUT SECTION#######    
@@ -330,7 +330,7 @@ function(input, output, session) {
 #      color = "yellow" 
 #    )
 #  })
-  
+#  
 #  output$timetoComplete <- renderValueBox({
 #    invalidateLater(1000, session) 
 #    elapsed <- as.numeric(Sys.time()) - startTime #Assumes a constant value to complete the job
