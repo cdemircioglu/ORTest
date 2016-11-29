c915881a31e61ca02161a046587a83927068fd45
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-11-29 15:22:21 +0800

    Code update

--- pull_ui.sh
+++ pull_ui.sh
@@ -2,4 +2,5 @@ git -C ~/ui/ stash save --keep-index
 git -C ~/ui/ stash drop
 git -C ~/ui/ fetch origin
 git -C ~/ui/ merge origin/master
+sed -i -e 's/8091/8191/g' server.R
 sudo rsync * /opt/shiny-server/openroads/
