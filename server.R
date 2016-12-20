c97debc983a0e9993042929b36593c88f27c869e
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-12-20 18:09:43 +0800

    Code update

--- server.R
+++ server.R
@@ -387,8 +387,8 @@ function(input, output, session) {
         arrange(desc(cmsisdn),desc(bucket)) #%>%
         # Just show the top 60, otherwise it gets hard to see
         #head(30)
-        mecon <- max(df$bucket)
-        df[df$bucket>0,1] <- floor(df$bucket*log(df$bucket)/log(mecon))
+        #mecon <- max(df$bucket)
+        #df[df$bucket>0,1] <- floor(df$bucket*log(df$bucket)/log(mecon))
         
         df<- arrange(df,desc(bucket),desc(cmsisdn))
         #write.csv(df, file = "aaa.csv", row.names = TRUE)
